0cba3198b5fd05ed155f5be39bd87946d7913d86
amit-gshe <scybhe@163.com>
2018-04-06 16:04:16 +0800

    Load time weaving

--- src/main/kotlin/com/example/aspectjdemo/AspectjDemoApplication.kt
+++ src/main/kotlin/com/example/aspectjdemo/AspectjDemoApplication.kt
@@ -1,14 +1,16 @@
 package com.example.aspectjdemo
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.ApplicationArguments
+import org.springframework.context.annotation.EnableLoadTimeWeaving
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured
 
 @SpringBootApplication
+@EnableLoadTimeWeaving
 @EnableSpringConfigured
-class AspectjDemoApplication:ApplicationRunner{
+class AspectjDemoApplication : ApplicationRunner {
 	override fun run(args: ApplicationArguments?) {
 		val person = Person()
 		person.sayHello()
